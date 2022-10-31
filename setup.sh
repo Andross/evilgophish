b8fb4baeca0bce2da284957635fb6711cf1e3a66
Andross <blake.balick.schreiber@gmail.com>
2022-10-31 16:39:13 -0400

    Updated setup.sh to perform auto config

--- setup.sh
+++ setup.sh
@@ -55,12 +55,13 @@ bl_bool="${7}"
 
 # Get path to certificates
 function get_certs_path () {
-    print_info "Run the command below to generate letsencrypt certificates (will need to create two (2) DNS TXT records):"
-    print_info "letsencrypt|certbot certonly --manual --preferred-challenges=dns --email admin@${root_domain} --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d '*.${root_domain}' -d '${root_domain}'"
-    print_info "Once certificates are generated, enter path to certificates:"
+    # print_info "Run the command below to generate letsencrypt certificates (will need to create two (2) DNS TXT records):"
+    # print_info "letsencrypt|certbot certonly --manual --preferred-challenges=dns --email admin@${root_domain} --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d '*.${root_domain}' -d '${root_domain}'"
+    # print_info "Once certificates are generated, enter path to certificates:"
+    certbot-auto --apache --domain ${root_domain}
     read -r certs_path
     if [[ ${certs_path: -1} != "/" ]]; then
-        certs_path+="/"
+        certs_path+="/etc/letsencrypt/live/${root_domain}/"
     fi
 }
 
