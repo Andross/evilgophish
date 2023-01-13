037afd437e925f18359ac70a5f58cf81af7c54be
Blake Balick-Schreiber <blake.balick.schreiber@gmail.com>
2023-01-13 14:35:13 -0500

    Update setup.sh
    Fixed issue with certs path

--- setup.sh
+++ setup.sh
@@ -61,7 +61,7 @@ function get_certs_path () {
     # certbot-auto --apache --domain ${root_domain}
     # read -r certs_path
     if [[ ${certs_path: -1} != "/" ]]; then
-        certs_path+="/evilgophish/certs"
+        certs_path+="/evilgophish/certs/"
     fi
 }
 
